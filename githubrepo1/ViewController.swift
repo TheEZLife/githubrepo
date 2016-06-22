e8fba0499ff98d14b9779d2eed99cc5955d11afa
Eli Zafrani <EZ@Elis-MacBook-Pro.local>
2016-06-21 21:21:25 -0400

    first commit

---
+++ githubrepo1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  githubrepo1
+//
+//  Created by Eli Zafrani on 6/21/16.
+//  Copyright (c) 2016 Eli. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
