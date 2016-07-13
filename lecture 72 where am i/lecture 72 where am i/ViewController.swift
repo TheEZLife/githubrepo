69a27fdd44f9d10c65bc286e094cb849f2a5811b
Eli Zafrani <EZ@nbp-245-178.nbp.ruw.rutgers.edu>
2016-07-13 12:40:56 -0400

    First commit

--- lecture 72 where am i/lecture 72 where am i/ViewController.swift
+++ lecture 72 where am i/lecture 72 where am i/ViewController.swift
@@ -7,14 +7,35 @@
 //
 
 import UIKit
+import CoreLocation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CLLocationManagerDelegate {
 
+    var manager: CLLocationManager!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        manager = CLLocationManager()
+        manager.delegate = self
+        manager.desiredAccuracy = kCLLocationAccuracyBest
+        manager.requestWhenInUseAuthorization()
+        manager.startUpdatingLocation()
+        
+        
+        
     }
 
+    func locationManager(manager: CLLocationManager!, didUpdateLocations locations: [AnyObject]!) {
+        
+        println(locations)
+        
+    }
+    
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
